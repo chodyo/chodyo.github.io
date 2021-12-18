fe7181aeed6d62b7199dbedd3216b31ceaacd296
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2021-12-18 16:56:29 -0500

    Move display as an attr on the data list

--- src/App.js
+++ src/App.js
@@ -9,24 +9,28 @@ const links = [
     faIcon: "github",
     iconActiveColor: "white",
     url: "https://www.github.com/chodyo",
+    display: "inline-block",
   },
   {
     name: "linkedin",
     faIcon: "linkedin-square",
     iconActiveColor: "#0073b1",
     url: "https://www.linkedin.com/in/cody-heffner/",
+    display: "inline-block",
   },
   {
     name: "twitter",
     faIcon: "twitter-square",
     iconActiveColor: "#1da1f2",
     url: "https://www.twitter.com/cnheffner",
+    display: "inline-block",
   },
   {
     name: "resume",
     faIcon: "file-text",
     iconActiveColor: "#3e94e4",
     url: "https://www.visualcv.com/cody-heffner",
+    display: "none",
   },
 ];
 
